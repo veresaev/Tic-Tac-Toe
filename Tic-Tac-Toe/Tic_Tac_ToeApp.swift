9f4e0e610a4ecceac1a0d0c44bb876c4f81a97f2
Sergei Veresaev <eslyue@gmail.com>
2023-07-04 23:26:06 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Sergey Veresaev on 4.7.23..
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
